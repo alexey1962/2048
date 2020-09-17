4a4119e6b07956f12ff5288cab97fc3ccacb2f59
Alexey <a.kashutin.dev@gmail.com>
2020-09-17 17:09:54 +0600

    Fix game.js

--- js/game.js
+++ js/game.js
@@ -1,32 +1,46 @@
+let createAndAppend = function({className, parentElement, value}, tag = 'div') {
+    let element = document.createElement(tag);
+    element.className = className;
+    if(value) {
+        element.innerHTML = value;
+    }
+    parentElement.appendChild(element);
+
+    return element;
+}
+
 class Game {
     constructor(parentElement, size = 4) {
-        let gameFieldElement = document.createElement('div');
-        gameFieldElement.className = 'game';
-        parentElement.appendChild(gameFieldElement);
+        let gameFieldElement = createAndAppend({
+            className: 'game',
+            parentElement
+        });
 
-        let headerElement = document.createElement('div');
-        headerElement.className = 'header';
-        gameFieldElement.appendChild(headerElement);
+        let headerElement = createAndAppend({
+            className: 'header',
+            parentElement: gameFieldElement
+        });
 
         this.rating = 0;
 
-        headerElement.innerHTML = 'Rating ' + this.rating;
+        headerElement.innerHTML = 'Raiting: ' + this.rating;
 
-        let fieldElement = document.createElement('div');
-        fieldElement.className = 'field';
-        gameFieldElement.appendChild(fieldElement);
+
+        let FieldElement = createAndAppend({
+            className: 'field',
+            parentElement: gameFieldElement
+        });
 
         for (let i = 0; i < size; i++) {
             for (let k = 0; k < size; k++) {
-                let cellElement = document.createElement('div');
-                cellElement.className = 'cell';
-
+                let cellElement = createAndAppend({
+                    className: 'cell',
+                    parentElement: FieldElement
+                });
 
                 if(Math.random() > 0.8) {
                     cellElement.innerHTML = Math.random() > 0.5 ? 4 : 2;
                 }
-
-                fieldElement.appendChild(cellElement);
             }
         }
     }
