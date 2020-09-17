325f64ffffcb6fad8c73f63ffe14c52869e427f1
Alexey <a.kashutin.dev@gmail.com>
2020-09-17 18:00:24 +0600

    Move createAndAppend helper to main.js and add cell class

--- js/game.js
+++ js/game.js
@@ -1,14 +1,3 @@
-let createAndAppend = function({className, parentElement, value}, tag = 'div') {
-    let element = document.createElement(tag);
-    element.className = className;
-    if(value) {
-        element.innerHTML = value;
-    }
-    parentElement.appendChild(element);
-
-    return element;
-}
-
 class Game {
     constructor(parentElement, size = 4) {
         let gameFieldElement = createAndAppend({
@@ -33,14 +22,7 @@ class Game {
 
         for (let i = 0; i < size; i++) {
             for (let k = 0; k < size; k++) {
-                let cellElement = createAndAppend({
-                    className: 'cell',
-                    parentElement: FieldElement
-                });
-
-                if(Math.random() > 0.8) {
-                    cellElement.innerHTML = Math.random() > 0.5 ? 4 : 2;
-                }
+                new Cell(FieldElement);
             }
         }
     }
